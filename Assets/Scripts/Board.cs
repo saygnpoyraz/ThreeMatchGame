0709cb512aaf681ed21f2e53cf6cb52f32a66fd4
saygnpoyrazm <saygnpoyraz@gmail.com>
2020-11-11 19:28:11 +0300

    Board Created

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+
+    public int width;
+    public int height;
+    public GameObject tilePrefab;
+    private BackgroundTile[,] allTiles; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        allTiles = new BackgroundTile[width,height];
+        SetUp();
+    }
+
+    private void SetUp()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                Instantiate(tilePrefab, new Vector2(i,j), Quaternion.identity);
+            }
+        }
+    }
+}
